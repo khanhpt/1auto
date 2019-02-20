45aeb82e3cc931a5ff5a8569952b61bea7345110
KhanhPt <phamtankhanh@gmail.com>
2019-02-20 10:26:18 +0700

    Add project files.

---
+++ 1Auto/1Auto/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using _1Auto.Models;
+
+namespace _1Auto.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
